bd459ee3d0bd4cf853fa939fd0783d8a0e89c60b
jadens-arc <arceneauxjaden@gmail.com>
2020-04-01 15:09:20 -0700

    now works with other displays besides HDMI-1

--- main.py
+++ main.py
@@ -12,11 +12,15 @@ root.geometry('50x100')
 root.resizable(False, False)
 root.wm_attributes("-topmost", 1)
 
+currentOutput = str(popen('xrandr | grep " connected" | cut -f1 -d " " ').read()).strip()
+print(currentOutput)
+
 def setBrightness(event):
     _level = level.get() / 100
+    print(_level)
     if _level <= .2:
         _level = .2
-    system(f'xrandr --output HDMI-1 --brightness {_level}')
+    system(f'xrandr --output {currentOutput} --brightness {_level}')
 
 level = Scale(root, from_=100, to_=0)
 level.set(float(popen("xrandr --verbose | awk '/Brightness/ { print $2; exit }'").read()) * 100)
